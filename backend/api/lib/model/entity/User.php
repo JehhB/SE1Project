88c6f2cbde01aab1798ee327019da84e4f71e17b
jonathan0066 <cunananjc600@gmail.com>
2024-01-15 18:11:22 +0800

    Implemented events, user, eventrepositoryimpl, userrepository impl

--- backend/api/lib/model/entity/User.php
+++ backend/api/lib/model/entity/User.php
@@ -2,24 +2,12 @@
 
 namespace Model\Entity;
 
-/**
- * @property string $userId ID corresponding to the user
- * @property string $name name for the user
- * @property string $email email of the user
- * @property string $passwordHash password hash of the user
- */
 class User {
     public string $userId;
     public string $name;
     public string $email;
     public string $passwordHash;
 
-    /**
-     * @param string $userId ID corresponding to the user
-     * @param string $name name for the user
-     * @param string $email email of the user
-     * @param string $passwordHash password hash of the user
-     */
     public function __construct(string $userId, string $name, string $email, string $passwordHash) {
         $this->userId = $userId;
         $this->name = $name;
@@ -27,14 +15,18 @@ class User {
         $this->passwordHash = $passwordHash;
     }
 
-    /**
-     * Function to send a json representation of user as response
-     * error code is 404 if $event is null, otherwise 200
-     * passwordHash is ommitted
-     * 
-     * @param ?User 
-     */
     public static function respond(?User $user): void {
-        // TODO: Pagawa
+        if ($user === null) {
+            http_response_code(404);
+            echo json_encode(['error' => 'User not found']);
+        } else {
+            http_response_code(200);
+            // Omitting the passwordHash property from the response
+            echo json_encode([
+                'userId' => $user->userId,
+                'name' => $user->name,
+                'email' => $user->email,
+            ]);
+        }
     }
 }
