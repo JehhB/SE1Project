f35bf3b83d3ff3ce88984e0d5c419357a48974c5
jonathan0066 <cunananjc600@gmail.com>
2024-01-16 18:43:24 +0800

    changed the autoloader, finiched todo in attendence and attendancerepository

--- backend/api/lib/model/repository/AttendanceRepositoryImpl.php
+++ backend/api/lib/model/repository/AttendanceRepositoryImpl.php
@@ -3,35 +3,83 @@
 namespace Model\Repository;
 
 use Model\Entity\Attendance;
+use PDO;
 
-class AttendanceRepositoryImpl implements AttendanceRepository {
+class AttendanceRepositoryImpl implements AttendanceRepository
+{
+    private PDO $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
     /**
-     * get attendance associated with attendance id
-     * 
-     * @param string $attendanceId id of the event to retrieve
-     * @return ?Attendance null if no attendance corresponding rallcall id, otherwise return specified event
+     * Get attendance associated with attendance id
+     *
+     * @param string $attendanceId Id of the event to retrieve
+     * @return ?Attendance|null Null if no attendance corresponding to the ID, otherwise return specified event
      */
-    public function getAttendance(string $attendanceId): ?Attendance {
+    public function getAttendance(string $attendanceId): ?Attendance
+    {
         // TODO:
-    }
+        $query = "SELECT * FROM Attendances WHERE attendanceId = :id";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':id', $attendanceId);
+        $statement->execute();
 
-    /** 
-     * add new attendance, and return eventId
-     * 
-     * @param Attendance $attendance to be inserted
-     * @return string id of newly inserted attendance
-    */
-    public function addAttendance(Attendance $attendance): string {
-        // TODO:
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if (!$result) {
+            return null; // Event not found
+        }
+
+        // Assuming you have a constructor in the Event class that sets properties
+        return new Attendance($result['attendanceId'], $result['registeredEventId'], $result['rollcallId'], $result['timestamp']);
     }
 
     /**
-     * get attendance of an registered event
-     * 
-     * @param string $registeredEventId id of the registered event to find attendance
+     * Add new attendance and return attendance ID
+     *
+     * @param Attendance $attendance To be inserted
+     * @return string Id of the newly inserted attendance
+     */
+    public function addAttendance(Attendance $attendance): string
+    {
+        // TODO:
+        $query = "INSERT INTO Attendances (attendanceId, registeredEventId, rollcallId,timestamp) VALUES (:attendanceId, :registeredEventId, :rollcallId,:timestamp)";
+        $id = uniqid();
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':attendanceId', $id);
+        $statement->bindParam(':registeredEventId', $attendance->registeredEventId);
+        $statement->bindParam(':rollcallId', $attendance->rollcallId);
+        $statement->bindParam(':timestamp', $attendance->timestamp);
+        $statement->execute();
+
+        // Return the last inserted ID
+        return $id;
+    }
+
+    /**
+     * Get attendance of a registered event
+     *
+     * @param string $registeredEventId Id of the registered event to find attendance
      * @return Attendance[]
      */
-    public function getAttendanceByRegisteredEvent(string $registeredEventId): array {
+    public function getAttendanceByRegisteredEvent(string $registeredEventId): array
+    {
         // TODO:
+        $query = "SELECT * FROM Attendances WHERE registeredEventId = :registeredEventId";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':registeredEventId', $registeredEventId);
+        $statement->execute();
+
+        $attendance = [];
+
+        while ($result = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $attendance[] = new Attendance($result['attendanceId'], $result['registeredEventId'], $result['rollcallId'], $result['timestamp']);
+        }
+
+        return $attendance;
     }
 }
